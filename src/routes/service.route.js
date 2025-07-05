49cff70149890dc5cc178123ddf05d1d260aea46
Minh Chuc <chucntmhe172296@fpt.edu.vn>
2025-07-05 20:07:22 +0700

    service funtion : find by name, create

--- src/routes/service.route.js
+++ src/routes/service.route.js
@@ -4,6 +4,7 @@ import serviceController from "../controllers/service.controller.js";
 const serviceRouter = express.Router();
 
 serviceRouter.get("/", serviceController.getAllServices);
+serviceRouter.get("/:name", serviceController.getServiceByName);
+serviceRouter.post("/", serviceController.createService);
 
-export default serviceRouter;
-
+export default serviceRouter
