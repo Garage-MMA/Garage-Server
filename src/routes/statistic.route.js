99f65f083e7a20b09e0229f324f33fecb85922e0
Minh Chuc <chucntmhe172296@fpt.edu.vn>
2025-07-08 23:22:40 +0700

    Add statistic feature

--- src/routes/statistic.route.js
+++ src/routes/statistic.route.js
@@ -7,4 +7,10 @@ statisticRouter.get("/:garageId/daily", statisticController.getDailyStatistic);
 
 statisticRouter.get("/:garageId/range", statisticController.getRangeStatistic);
 
+statisticRouter.get("/:garageId/weekly/:year/:week", statisticController.getWeeklyStatistic);
+
+statisticRouter.get("/:garageId/monthly/:year/:month", statisticController.getMonthlyStatistic);
+
+statisticRouter.get("/:garageId/yearly/:year", statisticController.getYearlyStatistic);
+
 export default statisticRouter;
