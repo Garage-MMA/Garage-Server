7660430ca67fcdcc270097e1d95ab8a08cb277fe
Minh Chuc <chucntmhe172296@fpt.edu.vn>
2025-07-09 16:55:27 +0700

    adđ new vehicle, update vehicle, delete vehicle

--- src/routes/vehicle.route.js
+++ src/routes/vehicle.route.js
@@ -4,8 +4,8 @@ import vehicleController from "../controllers/vehicle.controller.js";
 const vehicleRouter = express.Router();
 
 vehicleRouter.get("/my-vehicles", vehicleController.getMyVehicles);
-// vehicleRouter.post("/add-vehicle", vehicleController.addVehicle);
-// vehicleRouter.put("/update-vehicle/:licensePlate", vehicleController.updateVehicle);
-// vehicleRouter.delete("/delete-vehicle/:licensePlate", vehicleController.deleteVehicle);
+vehicleRouter.post("/add-vehicle", vehicleController.addVehicle);
+vehicleRouter.put("/update-vehicle/:licensePlate", vehicleController.updateVehicle);
+vehicleRouter.delete("/delete-vehicle/:licensePlate", vehicleController.deleteVehicle);
 
 export default vehicleRouter;
