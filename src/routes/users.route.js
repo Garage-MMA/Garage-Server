1b1777d916219023a3dee8028a0a5d9c6fe31d17
vintkhe176898 <vynguyenthikhanh5@gmail.com>
2025-07-10 17:38:47 +0700

    API edit profile

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userController from "../controllers/users.controller.js"; 
+import { verifyToken, verifyRole } from "../middleware/auth.middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", userController.register);
 userRouter.post("/login", userController.login);
+userRouter.put("/profile", verifyToken, userController.editProfile);
 
 export default userRouter;
